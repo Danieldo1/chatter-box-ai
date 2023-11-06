431e46cac68e1d99ae73bcb97bac891eea224185
Dan <daniel.speranskiy@live.com>
2023-11-06 14:46:30 +0400

    Made a dashboard page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { cn } from "@/lib/utils"
 import { LayoutDashboard, Bot, ImageIcon, VideoIcon, Music, Code, UserCog } from "lucide-react"
+import { usePathname } from "next/navigation"
 
 const montserrat = Montserrat({weight: '600', subsets: ['latin']})
 
@@ -52,6 +53,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname()
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
        <div className="px-3 py-2 flex-1">
@@ -71,7 +73,9 @@ const Sidebar = () => {
             <Link 
             href={route.href} 
             key={route.href}
-            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+            pathname=== route.href ? "bg-white/10" : 'bg-transparent'
+            )}
             >
             <div className="flex items-center flex-1">
                 <route.icon className={cn('w-5 h-5 mr-3',route.color  )} />
--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,15 @@ import { Menu } from "lucide-react"
 import { Button } from "./ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 import Sidebar from "./Sidebar"
+import { useEffect, useState } from "react"
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+    if(!isMounted) return null
   return (
 <Sheet>
     <SheetTrigger>
