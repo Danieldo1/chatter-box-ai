02cff367dbfded40166eacb114e5682ac74548fe
Dan <daniel.speranskiy@live.com>
2023-11-06 12:07:45 +0400

    Structuring the project for starting the project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard() {
+  return (
+    <main className="">
+     <p className="text-xl text-red-500">Hello</p>
+    </main>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Landing Page(unprotected)</h1>
+        </div>
+    )
+}
+
+export default LandingPage
